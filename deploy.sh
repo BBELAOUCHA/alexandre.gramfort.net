0ab5c5fd844af9a2778897214ab705bef1eb9b68
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-03-19 13:26:11 +0100

    fix for deploy

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,9 @@ perl -e "s/http:\/\/localhost\/agramfort\//./g;" -pi.save $(find deploy/ -type f
 find deploy -name "*.save" | xargs rm -f
 cp img/down.png deploy/img/down.png
 echo alexandre.gramfort.net > deploy/CNAME
+cp deploy/index.en.html deploy/index.html
+cp -r css deploy/
+cp -r js deploy/
 rm -rf localhost
 
 ghp-import -p deploy -r origin
