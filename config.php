885305ed0dedc8348ddd5023d9db6f3cdd58dd14
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-03-19 10:16:38 -0100

    Update documentation

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-ini_set('include_path',".:..:./inc:../inc:../../inc:../../../inc");
-if ($_SERVER['HTTP_HOST'] == "localhost")
-    $site_root = "http://localhost/agramfort";
-else
-    $site_root = "http://alexandre.gramfort.net";
-
-?>
--- po/scan_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-XGETTEXT=/usr/local/Cellar/gettext/0.18.1.1/bin/xgettext
-
-${XGETTEXT} ../*.php ../inc/*.inc.php --default-domain=webinria -L php --from-code UTF-8 --omit-header --sort-by-file --output=fr_FR.po -k_ -j
-${XGETTEXT}  ../*.php ../inc/*.inc.php --default-domain=webinria -L php --from-code UTF-8 --omit-header --sort-by-file --output=en_US.po -k_ -j
