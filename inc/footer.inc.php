824fd223123c14e5c0fedb5fdfcb8d6c957ff906
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-07-09 21:29:57 +0200

    urchin tracker pb due to localhost wget ...

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -24,14 +24,12 @@
     </div>
     <!-- end div.full -->
 
-    <?php  if ($_SERVER['HTTP_HOST'] != 'localhost') { ?>
     <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
     </script>
     <script type="text/javascript">
     _uacct = "UA-112258-9";
     urchinTracker();
     </script>
-    <?php } ?>
 
     </body>
 </html>
