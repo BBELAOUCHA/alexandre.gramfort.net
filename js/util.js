6e46cf359bd11d18bd0dd959409ac3b486a285cb
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-09-28 13:30:07 -0100

    Update documentation

--- js/util.js
+++ js/util.js
@@ -1,7 +1,7 @@
 function echo_email ()
 {
     var who_email = "alexandre.gramfort"; 
-    var who_domain = "inria.fr";
+    var who_domain = "telecom-paristech.fr";
     document.write("<a href=\"mailto"); 
     document.write(":" + who_email + "@"); 
     document.write(who_domain + "\">" + who_email + "@" + who_domain + "<\/a>");
